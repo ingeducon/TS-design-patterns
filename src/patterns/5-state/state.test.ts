fe324f7b6e0cadf4df90c72bce1f040d0682421c
ingeducon <ingeducon@gmail.com>
2022-03-02 17:55:16 -0500

    the state pattern has already been implemented

--- src/patterns/5-state/state.test.ts
+++ src/patterns/5-state/state.test.ts
@@ -1,6 +1,6 @@
 import { Context, ConcreteStateA, ConcreteStateB } from "./state";
 describe("state pattern", () => {
-  it("???", () => {
+  it("Cambio de estado", () => {
     const context = new Context(new ConcreteStateA());
     context.request1();
     const context2 = new Context(new ConcreteStateB());
