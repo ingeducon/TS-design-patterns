0f55ac22e00c373f9bcff80af39f53783b9ae6b8
ingeducon <ingeducon@gmail.com>
2022-03-02 12:13:05 -0500

    the facade pattern has already been implemented

--- src/patterns/3-facade/facade.test.ts
+++ src/patterns/3-facade/facade.test.ts
@@ -1,17 +1,25 @@
+import {
+  TV,
+  Lights,
+  SoundSystem,
+  TvState,
+  HomeAutomationFacade,
+} from "./facade";
+
 describe("facade pattern", () => {
   it("should run the movie scene for a home automated system", () => {
     const tv = new TV();
     const lights = new Lights();
     const soundSystem = new SoundSystem();
 
-    expect(tv.getState()).toEqual(DeviceState.OFF);
+    expect(tv.getState()).toEqual(TvState.Off);
     expect(lights.getLevel()).toEqual(0);
     expect(soundSystem.getVolume()).toEqual(0);
 
-    const homeAutomation = new HomeAutomation(tv, lights, soundSystem);
-    homeAutomation.runScene("movie");
+    const homeAutomation = new HomeAutomationFacade(soundSystem, lights, tv);
+    homeAutomation.watchMovie();
 
-    expect(tv.getState()).toEqual(DeviceState.ON);
+    expect(tv.getState()).toEqual(TvState.On);
     expect(lights.getLevel()).toEqual(40);
     expect(soundSystem.getVolume()).toEqual(100);
   });
--- src/patterns/3-facade/facade.ts
+++ src/patterns/3-facade/facade.ts
@@ -1,1 +1,77 @@
-// TODO
+export class SoundSystem {
+  private volume: number = 0;
+
+  setVolume(volumeLevel: number) {
+    this.volume = volumeLevel;
+  }
+  getVolume(): number {
+    return this.volume;
+  }
+}
+
+export class Lights {
+  private level: number = 0;
+
+  setLevel(level: number) {
+    this.level = level;
+  }
+  getLevel(): number {
+    return this.level;
+  }
+}
+
+export class TV {
+  private state: TvState = TvState.Off;
+  constructor() {
+    this.turnOff;
+  }
+
+  turnOn() {
+    this.state = TvState.On;
+  }
+
+  turnOff() {
+    this.state = TvState.Off;
+  }
+
+  getState(): TvState {
+    return this.state;
+  }
+}
+//----estados------
+
+export enum TvState {
+  On,
+  Off,
+}
+
+// ----
+export class HomeAutomationFacade {
+  private soundSystem: SoundSystem;
+  private lights: Lights;
+  private tv: TV;
+
+  constructor(soundSystem: SoundSystem, lights: Lights, tv: TV) {
+    this.soundSystem = soundSystem;
+    this.lights = lights;
+    this.tv = tv;
+  }
+
+  public watchMovie() {
+    this.tv.turnOn();
+    this.soundSystem.setVolume(100);
+    this.lights.setLevel(40);
+  }
+
+  endMovie() {
+    this.tv.turnOff();
+  }
+}
+
+/*
+let soundSystem = new SoundSystem();
+let lights = new Lights();
+let tv = new TV();
+
+let homeAutomation = new HomeAutomationFacade(soundSystem, lights, tv);
+homeAutomation.watchMovie();*/
